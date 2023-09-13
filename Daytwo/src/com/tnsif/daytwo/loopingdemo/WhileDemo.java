f72ea97c8dd1cd844617e792764ed75c3e55afe9
USER <USER@DESKTOP-G0FO4QD>
2023-09-13 18:26:44 +0530

    WhileDemo

---
+++ Daytwo/src/com/tnsif/daytwo/loopingdemo/WhileDemo.java
@@ -0,0 +1,15 @@
+package com.tnsif.daytwo.loopingdemo;
+
+public class WhileDemo {
+
+	public static void main(String[] args) {
+		int i=90;
+		while(i<=100)
+		{
+			System.out.println(i);
+			i++;
+		}
+
+	}
+
+}
